e69da6307f92910e9ae4157275ebd2de03f4d856
Ilya Krapivnoy <ilya.krapivnoy@gmail.com>
2021-04-15 14:37:40 +0300

    books to store

--- app.js
+++ app.js
@@ -10,20 +10,7 @@ class Book {
 // UI Class: Handle UI Tasks
 class UI {
     static displayBooks() {
-        const StoredBooks = [
-            {
-                title: 'Book One',
-                author: 'John Doe',
-                isbn: '3434434'
-            },
-            {
-                title: 'Book Two',
-                author: 'Jane Doe',
-                isbn: '45545'
-            },
-        ];
-
-        const books = StoredBooks;
+        const books = Store.getBooks();
 
         books.forEach((book) => UI.addBookToList(book))
     }
@@ -68,6 +55,36 @@ class UI {
 }
 
 // Store Class: Handles Storage
+class Store {
+    static getBooks() {
+      let books;
+      if(localStorage.getItem('books') === null) {
+        books = [];
+      } else {
+        books = JSON.parse(localStorage.getItem('books'));
+      }
+  
+      return books;
+    }
+  
+    static addBook(book) {
+      const books = Store.getBooks();
+      books.push(book);
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  
+    static removeBook(isbn) {
+      const books = Store.getBooks();
+  
+      books.forEach((book, index) => {
+        if(book.isbn === isbn) {
+          books.splice(index, 1);
+        }
+      });
+  
+      localStorage.setItem('books', JSON.stringify(books));
+    }
+  }
 
 // Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks)
@@ -92,7 +109,10 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
         // Add book to UI
         UI.addBookToList(book);
 
-        // Success message
+        // Add book to store
+        Store.addBook(book);
+
+        // Success alert
         UI.showAlert('Book Added', 'success')
     
         // Clear Fields
@@ -104,4 +124,7 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 // Event: Remove a Book
 document.querySelector('#book-list').addEventListener('click', (e) => {
     UI.deleteBook(e.target)
+    
+     // Success alert
+     UI.showAlert('Book Removed', 'success')
 })
